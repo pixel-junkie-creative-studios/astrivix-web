f3d8431aadf5a0e03ed02327d23850254a80ee7f
Pixel Junkie Creative Studios <pixeljunkiestudios.in@gmail.com>
2026-09-14 18:11:53 +0530

    fix(nav): enclose logo and toggle inside single floating oval glass bar and restore plus-to-cross animation

--- src/components/ui/StaggeredMenu.jsx
+++ src/components/ui/StaggeredMenu.jsx
@@ -441,8 +441,7 @@ export const StaggeredMenu = ({
           </span>
           <span ref={iconRef} className="sm-icon" aria-hidden="true">
             <span ref={plusHRef} className="sm-icon-line" />
-            <span ref={plusVRef} className="sm-icon-line" />
-            <span className="sm-icon-line" />
+            <span ref={plusVRef} className="sm-icon-line sm-icon-line-v" />
           </span>
         </button>
       </header>
